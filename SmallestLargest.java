32b30bec6645a3e0d294c998eb9c0b24d6199ade
Viv <harina2@cherrycreekschools.org>
2024-08-22 22:49:01 +0000

    pickles :O

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,30 @@
-//put your name here
+//Helen Arina 
 public class SmallestLargest{
     public static void main(String[] args){
-
+        int[] numbers;
+        numbers = new int[10];
+        for (int i = 0; i < 10; i++){
+            numbers[i] = (int) (Math.random() * 100) + 1;
+        }
+        System.out.println(smallestValue(numbers));
+        System.out.println(largestValue(numbers));
     }
-    public static int smallestValue(){
-
+    public static int smallestValue(int[] numbers){
+        int smallest = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < smallest){
+                smallest = numbers[i];
+            } 
+        }
+        return smallest;
     }
-    public static int largestValue(){
-        
+    public static int largestValue(int[] numbers){
+        int largest = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > largest){
+                largest = numbers[i];
+            } 
+        }
+        return largest;
     }
 }
